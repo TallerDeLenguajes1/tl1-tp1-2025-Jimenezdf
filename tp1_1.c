d90bf31bba94c846b6a22cbca728ed08d4298e7a
Facundo David Jimenez <fcndojmnz@gmail.com>
2026-04-13 01:00:22 -0300

    Ejercicio 4 completado

--- tp1_1.c
+++ tp1_1.c
@@ -1,9 +1,22 @@
-#include <stdio.h>
+#include  <stdio.h>
 #include <stdlib.h>
-// using namespace std
+
+int main(){
+    printf("hola mundo\n\n");
+
+    int *p;
+    int valor=3;
+    p=&valor;
+
+    printf("Contenido del puntero: %d\n", *p);
+    printf("Direccion de memoria almacenada por el puntero: %p\n", p);
+    printf("Direccion de memoria de la variable: %p\n", &valor);
+    printf("Direccion de memoria del puntero: %p\n", &p);
+    printf("Tamanio de memoria utilizado por la variable: %d bytes", sizeof(valor));
 
 
-void main()
-{
-    printf("hola mundo");
+
+
+
+    return 0;
 }
